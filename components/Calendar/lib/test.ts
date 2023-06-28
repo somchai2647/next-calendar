5d3bf80d9e345df4a56a5060f4beec44b66cbb32
somchai2647 <somchai.b@cvc.ac.th>
2023-06-28 15:43:06 +0700

    remove lib

--- components/Calendar/lib/test.ts
+++
@@ -1,9 +0,0 @@
-function A(x, value, y) {
-  if (value >= x && value <= y) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
-}
-
-A(1687431336000, 1687560936000, 1687647336000);
